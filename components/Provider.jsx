b43955e9b6025c817c4eb46484ce49d2e40374d2
AbhiAtiUttam <Abhi@glocalize.net>
2024-10-21 22:03:11 +0530

    google authentication done

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,6 @@
-"user client";
-
+"use client";
 import { SessionProvider } from "next-auth/react";
-import React from "react";
+
 
 const Provider = ({ children, session }) => {
   return <SessionProvider session={session}>{children}</SessionProvider>;
