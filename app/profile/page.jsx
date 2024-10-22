c9f8e5c8a4fd2f02ff0466cbad8729cb15578991
AbhiAtiUttam <Abhi@glocalize.net>
2024-10-22 11:33:32 +0530

    added profile page ,and route for get , patch and delete prompts

---
+++ app/profile/page.jsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Profile from "@components/profile";
+function MyProfile() {
+  const { data: session } = useSession();
+
+  const [posts , setPosts] = useState();
+
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if(session?.user.id) fetchPosts();
+  }, []);
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your presonalized profile page"
+      data={posts}
+      handelEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+}
+
+export default MyProfile;
